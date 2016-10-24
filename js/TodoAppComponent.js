ad8b15459fa43c2f2646b6f11b95356ee1810901
Yury Shulaev <yury.shulaev@gmail.com>
2016-10-24 21:43:46 +0300

    New API

--- js/TodoAppComponent.js
+++ js/TodoAppComponent.js
@@ -20,40 +20,40 @@ class TodoAppComponent extends SatoriComponent {
 		};
 	}
 
-	render() {
-		return this.view.section({class: 'todoapp'}, [
-			this.view.header({class: 'header'}, [
-				this.view.h1({}, 'todos'),
-				this.view.input({class: 'new-todo', attr: {placeholder: 'What needs to be done?', autofocus: ''},
+	render(h) {
+		return h('section', {class: 'todoapp'}, [
+			h('header', {class: 'header'}, [
+				h('h1', null, 'todos'),
+				h('input', {class: 'new-todo', attr: {placeholder: 'What needs to be done?', autofocus: ''},
 					keydown: this.view.inputKeyHandler(value => this.state.model.add(value), {reset: true}),
 				}),
 			]),
-			this.view.section({class: 'main', show: () => this.state.model.todos.length}, [
-				this.view.input({class: 'toggle-all', attr: {id: 'toggle-all', type: 'checkbox'},
+			h('section', {class: 'main', show: () => this.state.model.todos.length}, [
+				h('input', {class: 'toggle-all', attr: {id: 'toggle-all', type: 'checkbox'},
 					bind: {model: this.state.model, key: 'allCompleted'},
 				}),
-				this.view.label({attr: {for: 'toggle-all'}}, 'Mark all as complete'),
-				this.view.ul({class: 'todo-list', list: {array: () => this.state.items, item: todo =>
-					new TodoComponent(this.view, {model, todo}).render()
+				h('label', {attr: {for: 'toggle-all'}}, 'Mark all as complete'),
+				h('ul', {class: 'todo-list', list: {array: () => this.state.items, item: todo =>
+					new TodoComponent(this.view, {model, todo})
 				}}),
 			]),
-			this.view.footer({class: 'footer', show: () => this.state.model.todos.length}, [
-				this.view.span({class: 'todo-count'},
+			h('footer', {class: 'footer', show: () => this.state.model.todos.length}, [
+				h('span', {class: 'todo-count'},
 					() => [
-						this.view.strong({}, this.state.model.remaining.length), ' ',
+						h('strong', null, this.state.model.remaining.length), ' ',
 						this.view.pluralize('item', this.state.model.remaining.length), ' ', 'left',
 					]
 				),
-				this.view.ul({class: 'filters'},
+				h('ul', {class: 'filters'},
 					() => Array.from(this.view.unproxy(this.state.filters)).map(filter =>
-						this.view.li({},
-							this.view.a({class: {selected: () => filter[0] === this.state.filter},
+						h('li', null,
+							h('a', {class: {selected: () => filter[0] === this.state.filter},
 								attr: {href: '#/' + filter[0]}, content: filter[1].title,
 							})
 						)
 					)
 				),
-				this.view.button({class: 'clear-completed', show: () => this.state.model.completed.length,
+				h('button', {class: 'clear-completed', show: () => this.state.model.completed.length,
 					on: {click: () => this.state.model.clearCompleted()}, content: 'Clear completed',
 				}),
 			]),
--- js/TodoComponent.js
+++ js/TodoComponent.js
@@ -1,16 +1,16 @@
 'use strict'
 
 class TodoComponent extends SatoriComponent {
-	render() {
-		return this.view.li({class: {completed: () => this.state.todo.completed, editing: () => this.state.todo.editing}}, [
-			this.view.div({class: 'view'}, [
-				this.view.input({class: 'toggle', attr: {type: 'checkbox'},
+	render(h) {
+		return h('li', {class: {completed: () => this.state.todo.completed, editing: () => this.state.todo.editing}}, [
+			h('div', {class: 'view'}, [
+				h('input', {class: 'toggle', attr: {type: 'checkbox'},
 					bind: {model: this.state.todo, key: 'completed'},
 				}),
-				this.view.label({on: {dblclick: () => this.onLabelDblclick()}}, () => this.state.todo.title),
-				this.view.button({class: 'destroy', on: {click: () => this.state.model.remove(this.state.todo)}}),
+				h('label', {on: {dblclick: () => this.onLabelDblclick()}}, () => this.state.todo.title),
+				h('button', {class: 'destroy', on: {click: () => this.state.model.remove(this.state.todo)}}),
 			]),
-			this.edit = this.view.input({class: 'edit', on: {blur: () => this.saveEdit(this.state.todo, this.edit.value)},
+			this.edit = h('input', {class: 'edit', on: {blur: () => this.saveEdit(this.state.todo, this.edit.value)},
 				keydown: {
 					[this.view.Key.ENTER]: () => this.edit.blur(),
 					[this.view.Key.ESCAPE]: () => {this.state.todo.editing = false},
